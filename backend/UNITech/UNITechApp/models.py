464451e2aa06a4ff68251ca1d2cb9eb68f1e562a
gabri <gabrielf.santos@totvs.com.br>
2023-05-26 20:37:05 -0300

    Modelos de Matéria e Pesquisa

--- backend/UNITech/UNITechApp/models.py
+++ backend/UNITech/UNITechApp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Materia(models.Model):
+    nome = models.CharField('Nome da Matéria', max_length=100)
+
+    class Meta:
+        verbose_name = 'Matéria'
+        verbose_name_plural = 'Matérias'
+
+    def __str__(self):
+        return self.nome
+
+
+class Pesquisa(models.Model):
+    
+    class Meta:
+        verbose_name = 'Pesquisa'
+        verbose_name_plural = 'Pesquisas'
+
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
+    data_pesquisa = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Pesquisa de {self.usuario} em {self.materia}'
